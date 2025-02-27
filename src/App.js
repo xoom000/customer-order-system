3fcd82c265a3e4ac2dab876ddbe4747423477946
xoom000 <nigelwhaley@gmail.com>
2025-02-27 08:35:03 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AppProvider } from "./context/AppContext";
 import LandingPage from "./pages/LandingPage";
 import AdminPage from "./pages/AdminPage";
 import BuildCustomerPage from "./pages/BuildCustomerPage";
 import CustomerOrderPage from "./pages/CustomerOrderPage";
-import CustomerCard from '../components/CustomerCard';
+import RouteManagementPage from "./pages/RouteManagementPage";
+import NotificationToast from "./components/NotificationToast";
+import ApiStatusIndicator from "./components/ApiStatusIndicator";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/build-customer/:customerName" element={<BuildCustomerPage />} />
-        <Route path="/customer/:id" element={<CustomerOrderPage />} />
-        <Route path="/test" element={<TestCustomerTiles />} /> {/* Test route */}
-      </Routes>
-    </Router>
+    <AppProvider>
+      <Router>
+        <NotificationToast />
+        <ApiStatusIndicator />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/routes" element={<RouteManagementPage />} />
+          <Route path="/build-customer/:customerName" element={<BuildCustomerPage />} />
+          <Route path="/customer/:id" element={<CustomerOrderPage />} />
+        </Routes>
+      </Router>
+    </AppProvider>
   );
 }
 
